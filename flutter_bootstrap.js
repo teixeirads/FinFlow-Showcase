aa32658583ac381358015257657b56c91e5aef26
teixeirads <teixeirads@users.noreply.github.com>
2026-09-11 16:50:23 +0000

    deploy: teixeirads/FinFlow@2a0294c3c4e8568f51689530f41f98845baa9e04

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3949379951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3728119350" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
